cff485ca5a34658fd7a2e6e77b2020ad9f1343b3
Adam Alenius <adam.alenius@gmail.com>
2021-05-18 21:22:01 +0200

    add proper tokenation, still lacking boolean handling though

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,56 @@
-use std::env;
 use std::fs;
+
+fn tokenizer(json_as_string: String) -> Vec<String> {
+    let mut word_arr: Vec<String> = vec![];
+    let mut current_word = String::new();
+    let mut open_quote = false;
+    let mut open_digit = false;
+    
+    // TODO: handle booleans
+    for json_char in json_as_string.chars() {
+        match json_char {
+            '\n' | ' '  => (),
+            '\"' => {
+                if open_quote {
+                    word_arr.push(current_word);
+                    current_word = String::new();
+                    open_quote = false;
+                } else {
+                    // opening the word for writing
+                    open_quote = true;
+                }
+            }
+            new_char if open_quote => {
+                current_word.push(new_char);
+            }
+            digit if json_char.is_numeric() => {
+                if !open_digit {
+                    open_digit = true;
+                }
+                current_word.push(digit);
+            }
+            // separator is either , ] in array or } if it's last in object depending on position within the array
+            separator if open_digit => {
+                open_digit = false;
+                word_arr.push(current_word);
+                current_word = String::new();
+                word_arr.push(separator.to_string());
+            }
+            _ => {
+                // rest of the stuff, parentesis, brackets etc
+                word_arr.push(json_char.to_string());
+            }
+        }
+    }
+
+    return word_arr;
+
+}
 
 fn main() {
     let json_content = fs::read_to_string("./test_files/1.json")
         .expect("Something went wrong when reading the file");
 
-    let mut char_arr: Vec<char> = vec![];
-
-    // remove whitespace, newlines, quotes
-    for json_char in json_content.chars() {
-        match json_char {
-            ' ' | '\n' | '\"' => (),
-            _ => char_arr.push(json_char),
-        }
-    }
-
-    let mut token_arr: Vec<String> = vec![String::new()];
-    for ch in char_arr {
-        match ch {
-            ':' => token_arr.push(String::new()), // push empty str when splitting entry
-            _ => {
-                let last_elem = token_arr.last_mut().unwrap();
-                last_elem.push(ch)
-            }
-        }
-    }
-
-    println!("{:?}", token_arr);
+    let tokens = tokenizer(json_content);
+    println!("{:?}", tokens);
 }
