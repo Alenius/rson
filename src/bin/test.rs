b447e73f544af175a0e9be5a264e04c24ed6e558
Adam Alenius <adam.alenius@gmail.com>
2021-06-02 20:46:00 +0200

    remake rson as a rust lib

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,17 +1,13 @@
-use std::fs;
-
-mod lexer;
-mod parser;
-mod utils;
-mod types;
+use std::{ fs, env };
+use rson::{types::JsonObject, parse};
 
 fn main() {
+
     let json_content = fs::read_to_string("./test_files/1.json")
         .expect("Something went wrong when reading the file");
 
-    let tokens = lexer::lex(json_content);
-    let mut json_object = parser::parse(tokens);
     // println!("{:?}", json_object)
+    let mut json_object = parse(json_content);
     json_object.print();
     json_object.delete_key("strong");
     println!("----");
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use types::JsonObject;
+
+mod lexer;
+mod parser;
+mod utils;
+pub mod types;
+
+pub fn parse(json_content: String) -> JsonObject {
+    let tokens = lexer::lex(json_content);
+    let json_object = parser::parse(tokens);
+
+    return json_object
+}
