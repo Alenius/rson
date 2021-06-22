bf16c963744ca57e12b70e92e22f9a63e5d1c382
Adam Alenius <adam.alenius@gmail.com>
2021-06-22 19:58:25 +0200

    remove old parse and lex

--- src/parser.rs
+++ src/parser.rs
@@ -1,112 +1,213 @@
+use std::collections::HashMap;
 use std::slice::Iter;
-use std::vec;
-use super::utils::{typify_token, strip_quotes, is_string_token};
-use super::types::{JsonValue, JsonObject};
+
+use super::lexer::{Delimiters, JsonTokenType, Numbers, Token};
 
 
-
-fn parse_vec(iter: Iter<String>) -> (JsonValue, Iter<String>) {
-    let mut iter = iter.clone();
-    let mut arr: Vec<JsonValue> = vec![];
-    // TODO: break this out to handle nested arrays
-    while let Some(elem) = iter.next() {
-        if elem.eq("]") {
-            break;
-        }
-        if elem.eq(",") {
-            continue;
-        }
-        // nested vec
-        if elem.eq("[") {
-            let (parsed_vec, return_iter) = parse_vec(iter);
-            iter = return_iter;
-            arr.push(parsed_vec);
-            continue;
-        }
-
-        let typed_elem = typify_token(elem.to_owned());
-        arr.push(typed_elem);
-    }
-
-    return (JsonValue::Vec(arr), iter);
+#[derive(Debug)]
+pub struct JsonObject {
+    json: HashMap<String, JsonValue>,
 }
 
+impl JsonObject {
+    fn new() -> JsonObject {
+        return JsonObject {
+            json: HashMap::new(),
+        };
+    }
+    fn insert(&mut self, key: String, val: JsonValue) {
+        self.json.insert(key, val);
+    }
+}
 
-// returning an iter makes it easy to handle recursive case with nested objects 
-fn parse_tokens(token_iter: Iter<String>) -> (JsonObject, Iter<String>) {
-    let mut token_iter = token_iter;
-    let mut json_object = JsonObject::new();
+pub fn parse_tokens(iter: Iter<Token>) -> (Iter<Token>, JsonObject) {
+    let mut token_iter = iter.clone();
+    let mut object = JsonObject::new();
 
-    while let Some(token) = token_iter.next() {
-        let key: String;
-        // handle keys and things that comes first in the line
-        if token.eq(",") {
-            // comma separating the lines
-            continue;
-        } else if token.eq("}") {
-            // handling closing of an object.
-            return (json_object, token_iter)
-        } else {
-            // double check that the key value actually is a string
-            if !is_string_token(&token) {
-                panic!("The key is not a string, it's: {}", token);
-            }
+    let mut key: Option<String> = None;
 
-            let stripped_token = strip_quotes(token.to_owned());
-            key = stripped_token;
+    loop {
+        if let None = key {
+            key = get_key(&token_iter.next());
         }
 
-        // handle delimiters
-        if let Some(delimiter) = token_iter.next() {
-            if delimiter.ne(":") {
-                panic!("The delimiter is not a colon, it's: {}", delimiter);
-            }
-        }
-
-        // handle values
+        // check delimiter
         if let Some(token) = token_iter.next() {
-            match token.as_str() {
-                // object opened
-                "{" => {
-                    // returning the consumed iter
-                    let (object, return_iter) = parse_tokens(token_iter);
-                    token_iter = return_iter;
-                    json_object.insert(key, JsonValue::Object(object));
-                    continue;
+            if let JsonTokenType::Delimiter(Delimiters::Colon) = token.get_token() {
+                {}
+            } else {
+                panic!("Did not get colon after key, instead got: {:?}", token)
+            }
+        }
+
+        let token_value = token_iter.next().unwrap();
+
+        match token_value.get_token() {
+            // string
+            JsonTokenType::String(string) => {
+                object.insert(key.unwrap(), JsonValue::String(string));
+                key = None;
+            }
+            JsonTokenType::Number(num) => {
+                match num {
+                    Numbers::Integer(integer) => {
+                        object.insert(key.unwrap(), JsonValue::Num(JsonNum::Int(integer)))
+                    }
+                    Numbers::Float(float) => {
+                        object.insert(key.unwrap(), JsonValue::Num(JsonNum::Float(float)))
+                    }
                 }
-                // object closed
-                "}" => {
-                    return (json_object, token_iter);
-                }
-                // handle array
-                "[" => {
-                    let (parsed_arr, iterator) = parse_vec(token_iter);
-                    token_iter = iterator; // give the iterator back
-                    json_object.insert(key.to_owned(), parsed_arr);
-                    continue;
-                }
-                _ => {
-                    // handle numbers, strings and booleans
-                    let value = typify_token(token.to_owned());
-                    json_object.insert(key.to_owned(), value);
+                key = None;
+            }
+            JsonTokenType::Boolean(bool) => {
+                object.insert(key.unwrap(), JsonValue::Bool(bool));
+                key = None;
+            }
+            JsonTokenType::Null => {
+                object.insert(key.unwrap(), JsonValue::Null);
+                key = None;
+            }
+            JsonTokenType::Delimiter(del) => {
+                match del {
+                    // object open
+                    Delimiters::LeftBrace => {
+                        let (partly_consumed_iter, nested_object) = parse_tokens(token_iter);
+                        token_iter = partly_consumed_iter;
+                        object.insert(key.unwrap(), JsonValue::Object(nested_object));
+                        key = None;
+                    }
+                    Delimiters::RightBrace => panic!("Stray right brace"),
+                    // array
+                    Delimiters::LeftBracket => {
+                        let mut vec: Vec<JsonValue> = vec![];
+                        loop {
+                            let arr_val = token_iter.next();
+
+                            if arr_val.is_none() {
+                                panic!("Unexpected end of array");
+                            }
+                            match arr_val.unwrap().get_token() {
+                                JsonTokenType::String(string) => {
+                                    vec.push(JsonValue::String(string))
+                                }
+                                JsonTokenType::Number(num) => match num {
+                                    Numbers::Integer(integer) => {
+                                        vec.push(JsonValue::Num(JsonNum::Int(integer)))
+                                    }
+                                    Numbers::Float(float) => {
+                                        vec.push(JsonValue::Num(JsonNum::Float(float)))
+                                    }
+                                },
+                                JsonTokenType::Boolean(bool) => vec.push(JsonValue::Bool(bool)),
+                                JsonTokenType::Null => vec.push(JsonValue::Null),
+                                JsonTokenType::Delimiter(del) => {
+                                    match del {
+                                        Delimiters::Comma => continue,
+                                        Delimiters::RightBracket => {
+                                            object.insert(key.unwrap(), JsonValue::Vec(vec));
+                                            key = None;
+                                            break;
+                                        }
+                                        // object inside array
+                                        Delimiters::LeftBrace => {
+                                            let (partly_consumed_iter, nested_object) =
+                                                parse_tokens(token_iter);
+                                            token_iter = partly_consumed_iter;
+                                            vec.push(JsonValue::Object(nested_object));
+
+                                            let next_token = token_iter.next();
+                                            if let Some(token) = next_token {
+                                                if let JsonTokenType::Delimiter(
+                                                    Delimiters::RightBracket,
+                                                ) = token.get_token()
+                                                {
+                                                    return (token_iter, object);
+                                                } else {
+                                                    panic!(
+                                                        "Array is not finished with right bracket"
+                                                    )
+                                                }
+                                            }
+                                        }
+                                        Delimiters::RightBrace => {
+                                            // should already have been consumed by the object iter
+                                            panic!("Did not expect lonely right brace in array")
+                                        }
+                                        Delimiters::LeftBracket => {}
+                                        _ => panic!("Not implemented nested arrays yet"),
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Delimiters::RightBracket => panic!("Did not expect a single right bracket"),
+                    Delimiters::Comma | Delimiters::Colon => continue,
                 }
             }
+        }
+
+        let finish_token = token_iter.next();
+        if let Some(token) = finish_token {
+            if let JsonTokenType::Delimiter(Delimiters::RightBrace) = token.get_token() {
+                // end of object
+                return (token_iter, object);
+            }
+            if let JsonTokenType::Delimiter(Delimiters::Comma) = token.get_token() {
+                continue;
+            } else {
+                panic!(
+                    "Key value pair did not end in comma or end of input: {:?}",
+                    token
+                )
+            }
         } else {
-            panic!("Created a key, but there is no value. Unbalanced JSON.")
+            // token_iter will be depleted so this might be handled better
+            return (token_iter, object);
+        }
+    }
+}
+
+pub fn parse(insert_vec: Vec<Token>) -> JsonObject {
+    let mut token_vec = insert_vec.clone();
+
+    let first_token = token_vec.remove(0);
+    if first_token
+        .get_token()
+        .ne(&JsonTokenType::Delimiter(Delimiters::LeftBrace))
+    {
+        panic!(
+            "Object does not start with left brace, instead I got: {:?}",
+            first_token
+        );
+    }
+    let last_token = token_vec.pop();
+    if let Some(last_token) = last_token {
+        if last_token
+            .get_token()
+            .ne(&JsonTokenType::Delimiter(Delimiters::RightBrace))
+        {
+            panic!(
+                "Object does not end with right brace, instead I got: {:?}",
+                first_token
+            );
         }
     }
 
-    return (json_object, token_iter);
-}
-
-pub fn parse<'a>(lexed_json: Vec<String>) -> JsonObject {
-    let mut lexed_json = lexed_json.clone();
-    // TODO: check that these are correct.
-    lexed_json.pop();
-    lexed_json.remove(0);
-
-    let token_iter = lexed_json.iter();
-    let (json_object, _) = parse_tokens(token_iter);
+    let token_iter = token_vec.iter();
+    let (_, json_object) = parse_tokens(token_iter);
 
     return json_object;
+}
+
+fn get_key(next_val: &Option<&Token>) -> Option<String> {
+    if let Some(next_key) = next_val {
+        let key = next_key.get_token();
+        if let JsonTokenType::String(val) = key {
+            return Some(val);
+        } else {
+            panic!("Expected key to be string, instead got: {:?}", key)
+        }
+    } else {
+        panic!("Unexpected end of object");
+    }
 }
