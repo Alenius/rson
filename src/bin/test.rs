8ea34368548de31f48de1ff6b9ab375a9173e99d
Adam Alenius <adam.alenius@gmail.com>
2021-06-23 12:16:37 +0200

    fix manual test file

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,13 +1,10 @@
-use rson::{parse, parse_ann};
+use rson::{parse};
 use std::fs;
 
 fn main() {
-    let json_content = fs::read_to_string("./test_files/1.json")
+    let json_content = fs::read_to_string("./test_files/test_package.json")
         .expect("Something went wrong when reading the file");
 
-        println!("json {}", json_content);
-
-    parse_ann(json_content);
-    // json_object.print();
-    // println!("{}", json_object.get_value("object").unwrap())
+    let obj = parse(json_content);
+    println!("Res: {:?}", obj);
 }
