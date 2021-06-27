f96c297369931085e19c88f8ce70431adac6e0c0
Adam Alenius <adam.alenius@gmail.com>
2021-06-27 15:50:12 +0200

    add unit tests for the entire lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,48 @@ pub fn parse(json_content: String) -> JsonObject {
     let tokens = lexer::lex(json_content);
     let json_object = parser::parse(tokens);
 
-    return json_object
+    return json_object;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn parse_string_only() {
+        let str = "{
+            \"string\": \"I am a string\",
+            \"str_with_num\": \"I am a string w1th numb3rs\"
+        }";
+        println!("{:?}", parse(str.to_owned()));
+        parse(str.to_owned());
+    }
+
+    #[test]
+    fn parse_vec() {
+        let str = "{
+            \"array\": [1, 2, 3]
+        }";
+        parse(str.to_owned());
+    } 
+
+    #[test]
+    fn parse_nested_vec() {
+        let str = "{
+            \"array\": [[1, 2], [3, 4]]
+        }";
+        parse(str.to_owned());
+    }
+
+
+    #[test]
+    #[should_panic(expected="Unexpected end of object")]
+    fn panic_on_obj_ending_with_comma() {
+        let str = "{
+           \"key\": \"ending in comma\",
+        }";
+        parse(str.to_owned());
+    }
+
 }
